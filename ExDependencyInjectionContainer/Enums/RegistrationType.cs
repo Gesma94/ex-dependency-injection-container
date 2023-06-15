a187ad676115c1950d51824fea58b38b05f7cbfd
Gesma94 <46707463+Gesma94@users.noreply.github.com>
2023-06-16 01:37:36 +0200

    F!! Add solution and projects

---
+++ ExDependencyInjectionContainer/Enums/RegistrationType.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) 2023 - Gesma94
+// This code is licensed under MIT license (see LICENSE for details)
+
+namespace ExDependencyInjectionContainer.Enums
+{
+    internal enum RegistrationType
+    {
+        Singleton = 0,
+        Scoped = 1,
+        Transient = 2
+    }
+}
---
+++ ExDependencyInjectionContainer/Models/RegisteredService.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) 2023 - Gesma94
+// This code is licensed under MIT license (see LICENSE for details)
+
+using ExDependencyInjectionContainer.Enums;
+
+namespace ExDependencyInjectionContainer.Models
+{
+    internal record RegisteredService(Type InterfaceType, Type ImplementationType, RegistrationType RegistrationType);
+}
