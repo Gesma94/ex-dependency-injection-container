a5bd48082ab69228f4e888d86054255cdd8c2b08
Gesma94 <46707463+Gesma94@users.noreply.github.com>
2023-06-16 15:08:06 +0200

    E Make project visible to Tests project

---
+++ ExDependencyInjectionContainer/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿// Copyright (c) 2023 - Gesma94
+// This code is licensed under MIT license (see LICENSE for details)
+
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("ExDependencyInjectionContainer.Tests")]
