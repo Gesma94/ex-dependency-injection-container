9566149b4a2f63c333dff5a18828f94956cf206a
Gesma94 <46707463+Gesma94@users.noreply.github.com>
2023-06-16 15:07:56 +0200

    F!! Implements AddScoped and AddSingleton

---
+++ ExDependencyInjectionContainer/Core/DiBuilder.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) 2023 - Gesma94
+// This code is licensed under MIT license (see LICENSE for details)
+
+using ExDependencyInjectionContainer.Models;
+
+namespace ExDependencyInjectionContainer.Core
+{
+    public class DiBuilder
+    {
+        private readonly IList<RegisteredService> _registeredServiceCatalog = new List<RegisteredService>();
+
+        public DiBuilder AddSingleton<TInterface, TImplementation>() where TImplementation : TInterface
+        {
+            _registeredServiceCatalog.Add(new RegisteredService(typeof(TInterface), typeof(TImplementation), Enums.RegistrationType.Singleton));
+            return this;
+        }
+
+        public DiBuilder AddScoped<TInterface, TImplementation>() where TImplementation : TInterface
+        {
+            _registeredServiceCatalog.Add(new RegisteredService(typeof(TInterface), typeof(TImplementation), Enums.RegistrationType.Scoped));
+            return this;
+        }
+
+        internal IEnumerable<RegisteredService> GetServices()
+        {
+            return _registeredServiceCatalog;
+        }
+    }
+}
